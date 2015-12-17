af6c91e38c3707e557bdb6e4a26f000038aa2374
bonattt <bonattt@rose-hulman.edu>
2015-12-17 18:30:10 -0500

    working on tests

--- src/testing/TestErrThang.java
+++ src/testing/TestErrThang.java
@@ -4,43 +4,34 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
-import org.objectweb.asm.Opcodes;
+import org.junit.Test;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
-
-import org.junit.Test;
-
+import org.objectweb.asm.Opcodes;
 
 import csse374project.ClassDeclarationVisitor;
+import csse374project.DesignParser;
 
-public class UnitTestsDeclarationVisitor {
+public class TestErrThang {
 
 	private static String className = "java.lang.String";
 	
-	
 	@Test
-	public void test() {
+	public void testErrThang() {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
 		
-		
-		ClassVisitor vis = new ClassDeclarationVisitor(Opcodes.ASM5);
-		ClassReader reader = null;
 		try {
-			reader = new ClassReader(className);
+			DesignParser.main(new String[]{className});
 		} catch (IOException e) {
-			fail("IOException thrown by ClassReader");
+			fail("An IO-exception was thrown.");
 		}
-		reader.accept(vis, ClassReader.EXPAND_FRAMES);
-		
 		
 		String result = out.toString();
 		String expected = "more than just hello";
 		assertEquals(expected, result);
-		
 	}
 
 }
