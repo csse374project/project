9a050ee5520550ad5d25e086e08dd2100ee012d2
bonattt <bonattt@rose-hulman.edu>
2015-12-18 14:35:13 -0500

    trying to brain out the tests :-1:

--- src/testing/UnitTestsFieldVisitor.java
+++ src/testing/UnitTestsFieldVisitor.java
@@ -11,6 +11,7 @@ import org.junit.After;
 import org.objectweb.asm.Opcodes;
 
 import classRepresentation.Class;
+import classRepresentation.Field;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
@@ -19,6 +20,7 @@ import org.junit.Test;
 
 
 import csse374project.ClassDeclarationVisitor;
+import csse374project.ClassFieldVisitor;
 import interfaces.IClass;
 import interfaces.IField;
 import interfaces.IMethod;
@@ -30,37 +32,105 @@ public class UnitTestsFieldVisitor {
 	
 	@Before
 	public void setup() throws IOException {
-		// TODO fix this.
+		// TODO fix this.???
 		currentClass = new Class();
 		ClassReader reader = null;
 		reader = new ClassReader(className);
-		ClassVisitor vis = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
-		reader.accept(vis, ClassReader.EXPAND_FRAMES);
+		ClassVisitor decVis = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fVis = new ClassFieldVisitor(Opcodes.ASM5, decVis, currentClass);
+		reader.accept(fVis, ClassReader.EXPAND_FRAMES);
 	}
 
 	@Test
 	public void testCorrectNumberOfFields() {
 		List<IField> fields = currentClass.getFields();
-		assertEquals(1, fields.size());
+		assertEquals(5, fields.size());
 	}
 	@Test
-	public void testCorrectlyNamedField() {
+	public void testCorrectFields() {
 		List<IField> fields = currentClass.getFields();
-		IField result =	fields.get(0);
-		assertEquals("CASE_INSENSITIVE_ORDER", result.getName());
+		List<String> allNames = new ArrayList(5);
+		String[] expected = new String[]{"CASE_INSENSITIVE_ORDER", "value",
+				"hash", "serialVersionUID", "serialPersistentFields"};
+		for (int i = 0; i < fields.size(); i++) {
+			allNames.add(fields.get(i).getName());
+		}
+		
+		for (int i = 0; i < expected.length; i++) {
+			assertTrue("missing field " + expected[i],
+					allNames.contains(expected[i]));
+		}
 	}
 	@Test
 	public void testCorrectNonAccessModifiers() {
 		IField result =	currentClass.getFields().get(0);
-		List<String> mods = result.getNonAccessModifiers();
-		assertEquals(1, mods.size());
-		assertEquals("static", mods.get(0));
+		
+		fail("unimplimented");
 	}
 	@Test
-	public void testCorrectAccessModifiers() {
-		IField result =	currentClass.getFields().get(0);
-		char mod = result.getVisibility();
-		assertEquals('+', mod);
+	public void testCorrectAccessModifiersHash() {
+		String expectedName = "hash";
+		char expectedVisibility = '-';
+		List<IField> result = currentClass.getFields();
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getName().equals(expectedName)){
+				assertEquals(expectedVisibility, result.get(i).getVisibility());
+				return;
+			}
+		}
+		fail("did not find expected field \"hash\"");
+	}
+	@Test
+	public void testCorrectAccessModifiersValue() {
+		String expectedName = "value";
+		char expectedVisibility = '-';
+		List<IField> result = currentClass.getFields();
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getName().equals(expectedName)){
+				assertEquals(expectedVisibility, result.get(i).getVisibility());
+				return;
+			}
+		}
+		fail("did not find expected field \"value\"");
+	}
+	@Test
+	public void testCorrectAccessModifiersSerialUID() {
+		String expectedName = "serialVersionUID";
+		char expectedVisibility = '-';
+		List<IField> result = currentClass.getFields();
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getName().equals(expectedName)){
+				assertEquals(expectedVisibility, result.get(i).getVisibility());
+				return;
+			}
+		}
+		fail("did not find expected field \"serialVersionUID\"");
+	}
+	@Test
+	public void testCorrectAccessModifiersSerialPersistentFields() {
+		String expectedName = "serialPersistentFields";
+		char expectedVisibility = '-';
+		List<IField> result = currentClass.getFields();
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getName().equals(expectedName)){
+				assertEquals(expectedVisibility, result.get(i).getVisibility());
+				return;
+			}
+		}
+		fail("did not find expected field \"serialPersistentFields\"");
+	}
+	@Test
+	public void testCorrectAccessModifiersCaseOrder() {
+		String expectedName = "CASE_INSENSITIVE_ORDER";
+		char expectedVisibility = '+';
+		List<IField> result = currentClass.getFields();
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getName().equals(expectedName)){
+				assertEquals(expectedVisibility, result.get(i).getVisibility());
+				return;
+			}
+		}
+		fail("did not find expected field \"serialPersistentFields\"");
 	}
 	@Test
 	public void testCorrectType() {
