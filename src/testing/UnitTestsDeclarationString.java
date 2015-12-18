e9a116c60af13eafc20c23640436d5942abc15e5
bonattt <bonattt@rose-hulman.edu>
2015-12-17 21:28:44 -0500

    working on tests :+1:

--- src/testing/UnitTestsDeclarationString.java
+++ src/testing/UnitTestsDeclarationString.java
@@ -42,23 +42,30 @@ public class UnitTestsDeclarationString {
 		assertEquals("String", currentClass.getName());
 	}
 	@Test
+	public void testNumberOfInterfaces() {
+		// Strings implement: Serializable, Comparable<String>, CharSequence
+		List<String> interfaces = currentClass.getInterfaces();
+		assertEquals(3, interfaces.size());
+	}
+	
+	@Test
 	public void testInterfaces() {
 		// Strings implement: Serializable, Comparable<String>, CharSequence
-		// TODO add Comparable<String> to the test
 		List<String> interfaces = currentClass.getInterfaces();
 		List<String> expected = new ArrayList<String>();
-		expected.add("Serializable");
-		expected.add("Comparable<String>");
-		expected.add("CharSequence");
-		assertEquals(expected.size(), interfaces.size());
+		expected.add("java/io/Serializable");
+		expected.add("java/lang/Comparable");
+		expected.add("java/lang/CharSequence");
 		for (int i = 0; i < expected.size(); i++) {
-			assertTrue(interfaces.contains(expected.get(i)));
+			assertTrue(String.format("expected %s, interface %d/%d",
+					 expected.get(i), i+1, expected.size()),
+					interfaces.contains(expected.get(i)));
 		}
 	}
 	
 	@Test
 	public void testSuperClass() {
-		assertEquals("java.lang.Object", currentClass.getSuperClass());
+		assertEquals("java/lang/Object", currentClass.getSuperClass());
 	}
 
 }
