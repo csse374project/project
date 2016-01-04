525b073c0eea92d9d1b5ed3761677dd1b958f1be
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-01-04 17:56:22 -0500

    Fixed some tests
    Tests that correct methods and fields exist for a given class

--- src/testing/TestErrThang.java
+++ src/testing/TestErrThang.java
@@ -14,11 +14,16 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Opcodes;
 
 import csse374project.ClassDeclarationVisitor;
+import csse374project.ClassFieldVisitor;
+import csse374project.ClassMethodVisitor;
 import csse374project.DesignParser;
+import interfaces.IClass;
+import classRepresentation.Class;
+import classRepresentation.Classes;
 
 public class TestErrThang {
 
-	private static String className = "SampleClassForReadingInATest";
+	private static String className = "testing.SampleClassForReadingInATest";
 	private PrintStream stdout;
 	private ByteArrayOutputStream out;
 	
@@ -26,7 +31,7 @@ public class TestErrThang {
 	public void setup() {
 		stdout = System.out;
 		out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
+		//System.setOut(new PrintStream(out));
 	}
 	
 	@After
@@ -36,53 +41,130 @@ public class TestErrThang {
 
 	@Test
 	public void testHas_mutateSomething() throws IOException {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("+mutateSomething(int[]):void"));
-	}
-	@Test
-	public void testHas_finalStringMaker() throws IOException {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("-finalStringMaker(String[]):String"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		System.out.println(result);
+		assertTrue(result.contains("+ void mutateSomething(int[])"));
 	}
 	
 	@Test
-	public void testHas_identiry() throws IOException {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("<u>+identity(int):int<u/>"));
+	public void testHas_finalStringMaker() throws IOException {
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		System.out.println(result);
+		assertTrue(result.contains("- String finalStringMaker(java.lang.String[])"));
+	}
+	
+	@Test
+	public void testHas_identity() throws IOException {
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		assertTrue(result.contains("+ int identity(int)"));
 	}
 
 	@Test
 	public void testHas_useless() throws IOException  {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("<u>-useless():void<u/>"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		System.out.println(result);
+		assertTrue(result.contains("- void useless()"));
 	}
 
 	@Test
 	public void testHas_aBool() throws IOException  {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("+aBool:boolean"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		System.out.println(result);
+		assertTrue(result.contains("+ aBool: boolean"));
 	}
 	@Test
 	public void testHas_aChar() throws IOException  {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("-aChar:char"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		assertTrue(result.contains("- aChar: char"));
 	}
 	@Test
 	public void testHasSOME_WORD()  throws IOException {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("<u>-SOME_WORD:String<u/>"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		assertTrue(result.contains("+ SOME_WORD: java.lang.String"));
 	}
 	@Test
 	public void testHasSOME_CONSTANT()  throws IOException  {
-		DesignParser.main(new String[]{className});
-		String result = out.toString();
-		assertTrue(result.contains("<u>-SOME_CONSTANT:int<u/>"));
+		Classes classes = new Classes();
+		
+		IClass currentClass = new Class();
+		ClassReader reader = new ClassReader(className);
+		ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, currentClass);
+		ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, currentClass);
+		ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, currentClass);
+		reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+		classes.addClass(currentClass);
+		
+		String result = classes.toGraphViz();
+		assertTrue(result.contains("- SOME_CONSTANT: int"));
 	}
 }
