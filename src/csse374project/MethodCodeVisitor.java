032d87ada0f01d02a6bc6e4b2aac248279db12fc
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-01-05 16:28:09 -0500

    Added MethodCodeVisitor
    empty class

---
+++ src/csse374project/MethodCodeVisitor.java
@@ -0,0 +1,11 @@
+package csse374project;
+
+import org.objectweb.asm.MethodVisitor;
+
+public class MethodCodeVisitor extends MethodVisitor {
+
+	public MethodCodeVisitor(int arg0) {
+		super(arg0);
+	}
+
+}
