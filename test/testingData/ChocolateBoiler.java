17276faddab8528ab3dcd21122fa4c6fbbb6549e
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-01-21 17:01:29 -0500

    wrote chocolateBoiler for testing

---
+++ test/testingData/ChocolateBoiler.java
@@ -0,0 +1,50 @@
+package testingData;
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+	private volatile static ChocolateBoiler uniqueInstance;
+	
+	private ChocolateBoiler(){}
+	
+	public static ChocolateBoiler getInstance(){
+		if (uniqueInstance == null) {
+			synchronized (ChocolateBoiler.class) {
+				if (uniqueInstance == null) {
+					uniqueInstance = new ChocolateBoiler();
+				}
+			}
+		}
+		return uniqueInstance;
+	}
+	
+	public void fill(){
+		if (isEmpty()) {
+			empty=false;
+			boiled=false;
+			// fill the boiler with a milk/chocolate mixture
+		}
+	}
+	
+	public void drain() {
+		if (!isEmpty() && isBoiled()) {
+			//drain the boiled milk and chocolate
+			empty = true;
+		}
+	}
+	
+	public void boil(){
+		if (!isEmpty() && !isBoiled()) {
+			//bring the contents to a boil
+			boiled = true;
+		}
+	}
+	
+	public boolean isEmpty(){
+		return empty;
+	}
+	
+	public boolean isBoiled(){
+		return boiled;
+	}
+}
