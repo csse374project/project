43312d8d3f748439e7c8a574dd0dea83b612449a
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-01-27 16:43:29 -0500

    made DecoratorDetector (does not apply to Component)
    Every team member present.

---
+++ src/classRepresentation/designPaterns/DecoratorDetector.java
@@ -0,0 +1,47 @@
+package classRepresentation.designPaterns;
+
+import java.util.List;
+import java.util.Map;
+
+import classRepresentation.Classes;
+import classRepresentation.decorators.DecoratorDecorator;
+import classRepresentation.decorators.IClassDecorator;
+import interfaces.IClass;
+import interfaces.IField;
+
+public class DecoratorDetector {
+	
+	private Map<String, IClass> classMap;
+	
+	public DecoratorDetector(Classes classes) {
+		this.classMap = classes.getClasses();
+	}
+		
+	public void findDecorators() {
+		for(String className : classMap.keySet()) {
+			IClass currentClass = classMap.get(className);
+			checkForDecoratorPattern(currentClass);
+		}
+	}
+	
+	private void checkForDecoratorPattern(IClass clazz) {
+		String superClass = clazz.getSuperClass();
+		List<IField> fields = clazz.getFields();
+		for (int i = 0; i < fields.size(); i++) {
+			if (fields.get(i).getType().equals(superClass)) {
+				applyDecorator(clazz, superClass);
+				break;
+			}
+		}
+	}
+	private void applyDecorator(IClass clazz, String component) {
+		IClassDecorator decoratedClass = (IClassDecorator) clazz;
+		decoratedClass.decorate(new DecoratorDecorator(component));
+		
+	}
+	private void setComponentToInterface(String component) {
+		IClass compClass = classMap.get(component);
+		// TODO
+	}
+	
+}
