f72ece08cd76016ed80a128b64fcd33f0989d51b
bonattt <bonattt@rose-hulman.edu>
2016-02-17 16:11:21 -0500

    removed mysterious import

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -20,7 +20,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-import sun.security.krb5.internal.crypto.Des;
 import umlDiagram.UMLParser;
 
 public class MainWindow {
