e48147e3fcd4a5eb85eb3fea3dbe3feb45259832
bonattt <bonattt@rose-hulman.edu>
2016-02-18 18:23:44 -0500

    diddled with MakeConfigFile

--- src/gui/MakeConfigFile.java
+++ src/gui/MakeConfigFile.java
@@ -7,7 +7,10 @@ import java.util.Properties;
 
 public class MakeConfigFile {
 	
+	private String inputDirec, targetClasses, outputDirec, dotPath, phases, adapter, composite, decorator, singleton;
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO make this take console commands etc... to make a config file.
 		Properties config = new Properties();
 		config.setProperty("property1", "value 1");
 		config.setProperty("targetClasses", "java.lang.String java.util.ArrayList");
