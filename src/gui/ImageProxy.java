6758b5b8b24bd79712983494a26e8bfc1df54a27
bonattt <bonattt@rose-hulman.edu>
2016-02-18 17:35:26 -0500

    Debugging imageproxy delete picture (IP)

--- src/gui/ImageProxy.java
+++ src/gui/ImageProxy.java
@@ -4,19 +4,20 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.MediaTracker;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-public class LoadingProxy implements Icon {
+public class ImageProxy implements Icon {
 
 	volatile ImageIcon imageIcon;
 	final String imagePath;
 	Thread retrievalThread;
 	boolean retrieving = false;
 
-	public LoadingProxy(String path) {
+	public ImageProxy(String path) {
 		this.imagePath = path;
 	}
 
@@ -24,38 +25,41 @@ public class LoadingProxy implements Icon {
 	public void paintIcon(Component c, Graphics g, int x, int y) {
 		if (imageIcon != null && imageIcon.getImageLoadStatus() == MediaTracker.COMPLETE) {
 			imageIcon.paintIcon(c, g, x, y);
+			System.out.println("Testing");
 		}
 		else {
 			g.drawString("Reticulating splines...", x, y);
+			System.out.println("Wtf?");
 			if (!retrieving) {
 				retrieving = true;
 
+				System.out.println("Sup?");
+				
 				retrievalThread = new Thread(new Runnable() {
 					public void run() {
+						System.out.println("Yo!");
 						try {
 							File imageFile = new File(imagePath);
 							URL imageURL = null;
+							System.out.println("Hello!");
 							if (imageFile.exists()) {
 								imageURL = imageFile.toURI().toURL();
 							}
 							if (imageURL != null) {
 								imageIcon = new ImageIcon(imageURL);
+								System.out.println("Howdy!");
+								System.out.println("file deleted!: " + imageFile.delete());
 							}
 							if (imageIcon == null) {
 								retrieving = false;
 							}
-						} catch (Exception e) {
+						} catch (IOException e) {
 							e.printStackTrace();
 						}
 					}
 				});
 				retrievalThread.start();
 			}
-//			try {
-//				Thread.sleep(100);
-//			} catch (InterruptedException e) {
-//				// do nothing
-//			}
 		}
 		c.repaint();
 	}
